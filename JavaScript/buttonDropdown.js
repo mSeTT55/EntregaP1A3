6a7bee1993756ab33d1fd4915ba381842f4542c2
Mateus Sena Almeida <86931085+mSeTT55@users.noreply.github.com>
2023-05-16 16:18:52 -0300

    Ajuste de dropdow menu para mobile

--- JavaScript/buttonDropdown.js
+++ JavaScript/buttonDropdown.js
@@ -14,3 +14,21 @@ window.onclick = function(event) {
 function dropdown() {
   document.getElementById("dropdown-content").classList.toggle("show");
 }
+
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn2')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content2");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show2')) {
+        openDropdown.classList.remove('show2');
+      }
+    }
+  }
+}
+function dropdown2() {
+document.getElementById("dropdown-content2").classList.toggle("show2");
+}
