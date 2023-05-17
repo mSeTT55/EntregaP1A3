c2ea6bae3fba35c76d5830ace364f44e7bc5780a
Mateus Sena Almeida <86931085+mSeTT55@users.noreply.github.com>
2023-05-16 23:22:12 -0300

    Ajuste do JS

--- JavaScript/minhalista-Onclick-DesejoAssistir-Visto.js
+++ JavaScript/minhalista-Onclick-DesejoAssistir-Visto.js
@@ -1,4 +1,5 @@
 /*Função que ao clicar altera entre Desejo Assistir e visto*/
+let direita_itens_vazio = document.getElementById("direita-itens-vazio");
 let direita_Desejoassistir = document.getElementById("direita-itens-desejo-assistir");
 let direita_Visto = document.getElementById("direita-itens-visto");
 
@@ -6,13 +7,17 @@ let esquerda_Desejoassistir = document.getElementById("desejoassistir");
 let esquerda_Visto = document.getElementById("visto");
 
 function mudarTela_desejoassistir() {
-    if(direita_Visto.style.display === "flex"){
-        direita_Visto.style.display = "none"
+    if(direita_itens_vazio.style.display === "flex"){
+        direita_itens_vazio.style.display = "none";
+        direita_Visto.style.display = "none";
+        
         direita_Desejoassistir.style.display = "flex";
         esquerda_Visto.style.backgroundColor = "rgba(81, 71, 105, 0)";
         esquerda_Desejoassistir.style.backgroundColor = "rgba(81, 71, 105, 0.45)";
         
     }else{
+        direita_itens_vazio.style.display = "none";
+        direita_Visto.style.display = "none";
         direita_Desejoassistir.style.display = "flex";
         esquerda_Visto.style.backgroundColor = "rgba(81, 71, 105, 0)";
         esquerda_Desejoassistir.style.backgroundColor = "rgba(81, 71, 105, 0.45)";
@@ -22,13 +27,16 @@ function mudarTela_desejoassistir() {
 }
 
 function mudarTela_visto() {
-    if(direita_Desejoassistir.style.display === "flex"){
+    if(direita_itens_vazio.style.display === "flex"){
+        direita_itens_vazio.style.display = "none";
         direita_Desejoassistir.style.display = "none";
         direita_Visto.style.display = "flex";
         esquerda_Desejoassistir.style.backgroundColor = "rgba(81, 71, 105, 0)";
         esquerda_Visto.style.backgroundColor = "rgba(81, 71, 105, 0.45)";
 
     }else{
+        direita_itens_vazio.style.display = "none";
+        direita_Desejoassistir.style.display = "none";
         direita_Visto.style.display = "flex";
         esquerda_Desejoassistir.style.backgroundColor = "rgba(81, 71, 105, 0)";
         esquerda_Visto.style.backgroundColor = "rgba(81, 71, 105, 0.45)";
